32d787d43b547b5d8e87efa6a6a72d4402e77b5b
Youssef-Sa3d <youssef.saad33218@gmail.com>
2025-09-05 19:21:25 +0300

    Fix backend bugs

--- backend/app.js
+++ backend/app.js
@@ -41,8 +41,8 @@ app.use(
 );
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
 app.use("/auth", authRoutes);
